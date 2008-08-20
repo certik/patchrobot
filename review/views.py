79a5bd076a92f1a657a20e8af7e7f356273ae071
Ondrej Certik <ondrej@certik.cz>
2008-08-20 19:19:54 +0200

    Refactoring.

--- review/views.py
+++ review/views.py
@@ -4,9 +4,7 @@ from django.http import HttpResponse
 from patchrobot.review.models import Issue
 
 def index(request):
-    issue_list = []
-    for i in Issue.objects.all():
-        issue_list.append({"issue":i, "patch_set": i.patch_set.all()})
+    issue_list = Issue.objects.all()
     return render_to_response('review/index.html', {'issue_list': issue_list})
 
 def issue(request, issue_id):
