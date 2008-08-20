123c79ab0d9ffea3d3584aab44530a510652b53d
Ondrej Certik <ondrej@certik.cz>
2008-08-20 19:17:43 +0200

    Print stuff

--- review/views.py
+++ review/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponse
 from patchrobot.review.models import Issue
 
 def index(request):
-    issue_list = Issue.objects.all()
+    issue_list = []
+    for i in Issue.objects.all():
+        issue_list.append({"issue":i, "patch_set": i.patch_set.all()})
     return render_to_response('review/index.html', {'issue_list': issue_list})
 
 def issue(request, issue_id):
